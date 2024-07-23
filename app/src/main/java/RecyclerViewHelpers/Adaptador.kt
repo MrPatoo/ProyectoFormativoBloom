3b37055a69cedc3ec38f7927577009084a5fadb3
Rodrigo <20230333@ricaldone.edu.sv>
2024-07-22 21:18:23 -0600

    ya se pueden eliminar datos

--- app/src/main/java/RecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -2,7 +2,12 @@ package RecyclerViewHelpers
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
 import modelo.tbPaciente
 import rodrigo.cordova.hospitalbloom.R
 
@@ -31,6 +36,56 @@ class Adaptador(var Datos: List<tbPaciente>): RecyclerView.Adapter<ViewHolder>()
 
 
 
+        //TODO eLIMINAR DATOS-------------------------------------------------------------------------------------------------
+
+        fun eliminarDatos(nombre: String, position: Int){
+            val ListaDatos = Datos.toMutableList()
+            ListaDatos.removeAt(position)
+
+            GlobalScope.launch(Dispatchers.IO){
+                //crear objConexion
+                val objConexion = ClaseConexion().cadenaConexion()
+
+                //variable con el PrepareStatement
+                val deletePaciente = objConexion?.prepareStatement("delete from tbPaciente where nombre = ?")!!
+                deletePaciente.setString(1, nombre)
+                deletePaciente.executeUpdate()
+
+                val commit = objConexion.prepareStatement("commit")
+                commit.executeUpdate()
+            }
+        }
+
+
+
+        val paciente = Datos[position]
+        holder.txtNombreCard.text = paciente.nombre
+        holder.txtApellidoCard.text = paciente.apellido
+        holder.txtEdadCard.text = paciente.edad.toString()
+        holder.txtNumHCard.text = paciente.numHabitacion.toString()
+        holder.txtNumCCard.text = paciente.numCama.toString()
+        holder.txtFechaIngresoCard.text = paciente.fechaIngreso
+        holder.txtEnfermedadCard.text = paciente.enfermedad.toString()
+        holder.txtMedicamentoCard.text = paciente.medicamento.toString()
+
+        holder.btnEliminarCard.setOnClickListener{
+            //alert
+            val context = holder.itemView.context
+
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Eliminar")
+            builder.setTitle("¿Desea eliminar el ticket?")
+            //botones
+            builder.setPositiveButton("Si"){dialog, switch -> eliminarDatos(paciente. nombre, position)
+
+            }
+
+            builder.setNegativeButton("No"){dialog, switch -> dialog.dismiss()
+
+            }
+            val dialog = builder.create()
+            dialog.show()
+        }
 
 
 
